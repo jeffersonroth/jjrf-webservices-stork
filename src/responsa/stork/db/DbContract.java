2dba0b12711a95bd40691f501e7ed84989e04379
Jefferson Johannes Roth Filho <jjrothfilho@gmail.com>
2018-07-09 20:00:42 -0500

    Update DbContract.java

--- src/responsa/stork/db/DbContract.java
+++ src/responsa/stork/db/DbContract.java
@@ -1,9 +1,9 @@
 package responsa.stork.db;
 
 public interface DbContract {
-	public static final String HOST = "jdbc:postgresql://104.130.29.251:5432/";
-	public static final String DB_NAME = "responsadb";
-    public static final String USERNAME = "camilo";
-    public static final String PASSWORD = "camilo";
+	public static final String HOST = "jdbc:postgresql://******************/";
+	public static final String DB_NAME = "***************";
+    public static final String USERNAME = "*****";
+    public static final String PASSWORD = "*****";
 
 }
