6de52ff32d4ba5bd05774d091ef97c989c89fd54
Jefferson Johannes Roth Filho <jjrothfilho@gmail.com>
2018-07-09 20:11:52 -0500

    Delete DbContract.java

--- src/responsa/stork/db/DbContract.java
+++
@@ -1,9 +0,0 @@
-package responsa.stork.db;
-
-public interface DbContract {
-	public static final String HOST = "jdbc:postgresql://******************/";
-	public static final String DB_NAME = "***************";
-    public static final String USERNAME = "*****";
-    public static final String PASSWORD = "*****";
-
-}
